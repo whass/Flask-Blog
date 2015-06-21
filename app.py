cbc3bfc886e2dd250a2e0d6c1f3d9d9d5725eb5b
whassani <hassani.walid@gmail.com>
2015-06-21 06:01:10 +0200

    add login, logout and conncetion to database

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import Flask, flash, redirect, render_template, request, session, abort
 
+import os
+from sqlalchemy.orm import sessionmaker
+from dbMyBlog import *
+engine = create_engine('sqlite:///dbMyBlog.db', echo=True)
+ 
+app = Flask(__name__)
+ 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello Boss!"
+ 
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+
+    POST_USERNAME = str(request.form['username'])
+    POST_PASSWORD = str(request.form['password'])
+
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    result = query.first()
+    if result:
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()
 
 if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
     app.run()
 
