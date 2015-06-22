50c0d992d85baad7b32b58b4ddee98b0fcb9433d
whassani <hassani.walid@gmail.com>
2015-06-22 03:53:25 +0200

    add functions remove and update post and show only post

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
-
 import os
 from sqlalchemy.orm import sessionmaker
 from models import User, Post, create_engine
+from datetime import datetime
+
 engine = create_engine('sqlite:///dbMyBlog.db', echo=True)
  
 app = Flask(__name__)
@@ -17,19 +17,23 @@ def home():
         return "Hello Boss!"
  
 @app.route('/login', methods=['GET','POST'])
-def do_admin_login():
-    POST_USERNAME = str(request.form['username'])
-    POST_PASSWORD = str(request.form['password'])
+def login():
+    if session.get('logged_in'):
+        return "your are logged already"
 
-    Session = sessionmaker(bind=engine)
-    s = Session()
-    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
-    result = query.first()
-    if result:
-        session['logged_in'] = True
-    else:
-        flash('wrong password!')
-    return home()
+    if request.method == 'POST':
+	POST_USERNAME = str(request.form['username'])
+    	POST_PASSWORD = str(request.form['password'])
+
+    	Session = sessionmaker(bind=engine)
+    	s = Session()
+    	query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    	result = query.first()
+    	if result:
+             session['logged_in'] = True
+    	else:
+             flash('wrong password!')
+    return render_template('login.html')
 
 @app.route("/logout")
 def logout():
@@ -38,24 +42,61 @@ def logout():
 
 @app.route('/add/' , methods=['POST', 'GET'])
 def add():
-    if request.method == 'POST':
+    if not session.get('logged_in'):
+        return render_template('login.html')
+
+    if (request.method == 'POST'):
         post=Post(request.form['title'], request.form['body'])
 	
 	Session = sessionmaker(bind=engine)
 	s = Session()
 
-        s.add(post)
+	s.add(post)
         s.commit()
-        flash('New entry was successfully posted')     
-             
-    return render_template('add.html')
+    return render_template('add.html')	
 
+@app.route('/update/<int:post_id>' , methods=['POST', 'GET'])
+def update(post_id):
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    post=s.query(Post).get(post_id)
+
+    if (request.method == 'POST'):
+
+        post1=Post(request.form['title'], request.form['body'])
+        s.query(Post).filter_by(id=post_id).update({"title": post1.title, "body": post1.body, "updated_at": datetime.utcnow()})
+        s.commit()	
+    return render_template('update.html', post=post)	
+
+ 
 @app.route('/show/', methods=['POST', 'GET'])
 def show():  
-  Session = sessionmaker(bind=engine)
-  s = Session()
-  post = s.query(Post).all()
-  return render_template('blog.html', post=post)
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    post = s.query(Post).all()
+    return render_template('blog.html', post=post)
+
+@app.route('/show/<int:post_id>')
+def show_single(post_id):  
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    post = s.query(Post).get(post_id)
+    return render_template('blog_single.html', post=post)
+
+@app.route('/delete/<int:post_id>')
+def delete(post_id):  
+    if not session.get('logged_in'):
+        return render_template('login.html')
+ 
+    else: 
+	Session = sessionmaker(bind=engine)
+        s = Session()
+        post = s.query(Post).get(post_id)
+        s.delete(post) 
+        s.commit()
+        return "deleted"
+
+
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
